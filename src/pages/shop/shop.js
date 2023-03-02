7bd424c23b19790ea9aef043c36b4dd5aa444a62
davidharnisch <davidharnisch@seznam.cz>
2023-03-02 14:40:21 +0100

    Shopping cart finish

---
+++ src/pages/shop/shop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Products } from '../../products';
+import { Product } from './product';
+import "./shop.css";
+
+export const Shop = () => {
+    
+    return (
+      <div className="shop">
+        <div className="shopTitle">
+            <h1>My shop</h1>
+        </div>
+        <div className="products">
+            {Products.map((product) => (
+            <Product data={product} />
+            ))}
+        </div>
+      </div>
+    );
+  }
+  
